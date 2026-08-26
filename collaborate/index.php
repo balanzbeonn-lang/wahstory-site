3dbdaeaf66cc5278cabd766aee28291174cbe827
wahstory <web.wahstory@gmail.com>
2026-08-26 13:26:45 +0530

    Gcode chnage

--- collaborate/index.php
+++ collaborate/index.php
@@ -80,12 +80,12 @@ $postObj = new Story();
     }
     </script>
     <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-LVRFRRWSM2"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-HWRTJNT2RY"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
-      gtag('config', 'G-LVRFRRWSM2');
+      gtag('config', 'G-HWRTJNT2RY');
     </script>
     
     <style>
